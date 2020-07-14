8892e30d357e95b7e26a8551f382c3d491679fac
lkrsnik <krsnik.luka92@gmail.com>
2020-07-14 13:38:03 +0000

    Added run.sh script

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+languages=("CRO" "hr" "ENG" "en" "EST" "et" "FIN" "fi" "LAT" "lv" "RUS" "ru" "SLO" "sl" "SWE" "sv")
+len=$((${#languages[@]} / 2 - 1))
+# echo $len
+
+for i in $(seq 0 $len)
+do
+	python fasttext_ner.py --ner_data_path "../data/${languages[$(($i * 2))]}/cross_validation/" --model "model/cc.${languages[$(($i * 2 + 1))]}.300.bin"  --results_file "fasttext_base" | tee ../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base.log
+done
+
