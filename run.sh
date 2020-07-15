48aeb89ff1cacc385b09b62ce2b88bf75b43a886
lkrsnik <krsnik.luka92@gmail.com>
2020-07-15 08:26:00 +0000

    Modifications in run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ len=$((${#languages[@]} / 2 - 1))
 
 for i in $(seq 0 $len)
 do
-	python fasttext_ner.py --ner_data_path "../data/${languages[$(($i * 2))]}/cross_validation/" --model "model/cc.${languages[$(($i * 2 + 1))]}.300.bin"  --results_dir "../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base" | tee ../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base.log
+	if [ ! -d "../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base" ]; then
+		python fasttext_ner.py --ner_data_path "../data/${languages[$(($i * 2))]}/cross_validation/" --model "model/cc.${languages[$(($i * 2 + 1))]}.300.bin"  --results_dir "../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base" | tee ../data/${languages[$(($i * 2))]}/cross_validation/fasttext_base.log
+	fi
 done
 
